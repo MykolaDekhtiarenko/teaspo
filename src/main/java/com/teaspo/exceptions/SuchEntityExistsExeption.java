7fc10bb03c10c34a0db693c7a6b6b58169b5a5f7
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-03-28 17:08:42 +0300

    minor api changes

---
+++ src/main/java/com/teaspo/exceptions/SuchEntityExistsExeption.java
@@ -0,0 +1,7 @@
+package com.teaspo.exceptions;
+
+/**
+ * Created by mykola.dekhtiarenko on 30.12.16.
+ */
+public class SuchEntityExistsExeption {
+}
---
+++ src/main/java/com/teaspo/views/PlaceView.java
@@ -0,0 +1,7 @@
+package com.teaspo.views;
+
+/**
+ * Created by mykola.dekhtiarenko on 29.12.16.
+ */
+public class PlaceView {
+}
